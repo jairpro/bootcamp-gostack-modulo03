d37f6744f5c837b7d7853318e4cd980bfd894df7
jairpro <jairedias@yahoo.com.br>
2020-02-13 14:09:01 -0300

    Validações de agendamento
    Adicionada dependência "date-fns"
    
    Novas validações:
    - Check for past dates
    - Check data availability
    
    Salvando horas cheias ao agendar

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -28,10 +29,36 @@ class AppointmentController {
         .json({ error: 'You can olny create appointments with providers' });
     }
 
+    /**
+     * Check for past dates
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permited' });
+    }
+
+    /**
+     * Check data availability
+     */
+    const checkDateAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkDateAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not avaliable' });
+    }
+
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
 
     return res.json(appointment);
