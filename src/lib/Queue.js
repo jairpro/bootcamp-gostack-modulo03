a4c2bf8a383f491b7cd7e221751a37d2fa1386ad
jairpro <jairedias@yahoo.com.br>
2020-02-15 14:57:44 -0300

    Monitorando falhas na fila

--- src/lib/Queue.js
+++ src/lib/Queue.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import Bee from 'bee-queue';
 import CancelationMail from '../app/jobs/CancellationMail';
 import redisConfig from '../config/redis';
@@ -30,8 +31,12 @@ class Queue {
     jobs.forEach(job => {
       const { bee, handle } = this.queues[job.key];
 
-      bee.process(handle);
+      bee.on('failed', this.handleFailure).process(handle);
     });
+  }
+
+  handleFailure(job, err) {
+    console.log(`Queue ${job.queue.name}: FAILED`, err);
   }
 }
 
