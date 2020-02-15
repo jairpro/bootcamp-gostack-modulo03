a4f1c871e1f4e6b1f1b98e88f822da8b946e4f0d
jairpro <jairedias@yahoo.com.br>
2020-02-15 11:21:12 -0300

    Configurando fila com Redis
    Adaptando a aplicação de filas (src/queue.js) ao Dotenv pois serão utilizadas as variáveis de ambientes  das configurações da conta de e-mail (em .env) para o job de cancelamento de agendamento que envia o email para avisar o prestador

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -7,7 +7,8 @@ import File from '../models/File';
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
 
-import Mail from '../../lib/Mail';
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
   async index(req, res) {
@@ -162,19 +163,9 @@ class AppointmentController {
 
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      // html:
-      // text: 'Você tem um novo cancelamento',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "dd 'de' MMMM', às' H:mm'h'", {
-          locale: pt,
-        }),
-      },
+    await Queue.add(CancellationMail.key, {
+      appointment,
+      // teste: 'teste'
     });
 
     return res.json(appointment);
